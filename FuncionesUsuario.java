00cd66fcb51b306054f13529e7b90743adb8cfc1
Rodrigo Miranda <rodrigomc123321@gmail.com>
2020-04-05 20:32:27 -0300

    Primer commit de rama funciones usuario, calcula los impuestos para cada usuario

---
+++ FuncionesUsuario.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author rodri
+ */
+public class FuncionesUsuario {
+
+    public int CalcularImpuestos(Usuario u){
+        int impuesto = 0;
+        if(u.getEdad() < 18){
+            impuesto = 0;
+        }else if(u.getEdad() < 30){
+             impuesto = 2000;
+        }else if(u.getEdad() < 65){
+            impuesto = 7000;
+        }else if(u.getEdad() >= 65){
+            impuesto = 2000;
+        }
+        int descuentoHijos = u.getCantHijos()*3;
+        if(descuentoHijos > 50){
+            descuentoHijos = 50;
+        }
+        impuesto = impuesto*(descuentoHijos/100);
+        return impuesto;
+    }
+}
