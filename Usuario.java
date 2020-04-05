8581ebc291c0eb92e56404c0ab52379fd0bdf06b
Rodrigo Miranda <rodrigomc123321@gmail.com>
2020-04-05 20:28:02 -0300

    Primer commit de rama dominio, inicializa los valores

---
+++ Usuario.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author rodri
+ */
+public class Usuario {
+
+        String nombre;
+        String cedula;
+        int contraseña;
+        int edad;
+        int cantHijos;
+        int saldo = 0;
+          
+}
+
